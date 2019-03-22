440ce0982a5dad775dee9a21a89227c0961df865
Katie Blizzard <kblizzard11@gmail.com>
2019-03-22 01:53:59 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -45,11 +45,15 @@ class Owner
   end 
   
   def play_with_cats
-    
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end 
   end 
   
   def feed_fish
-    
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end 
   end 
   
   
